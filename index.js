9ec49d071025b1b5214d4b0a24d70c60883024b1
Jaskaran Sohi <jaskaran.sohi@gmail.com>
2021-11-23 09:13:17 -0800

    commit tree 253b63178652f27555c920071b76ed4562690b1f

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+  return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs((start - destination) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+  const free = 400;
+  let totalfeettravelled = distanceTravelledInFeet(start, destination);
+  if (totalfeettravelled <= free) return 0;
+  else if (totalfeettravelled > free && totalfeettravelled < 2000)
+    return (totalfeettravelled - 400) * 0.02;
+  else if (totalfeettravelled >= 2000 && totalfeettravelled < 2500) return 25;
+  else totalfeettravelled > 2500;
+  return "cannot travel that far";
+}
